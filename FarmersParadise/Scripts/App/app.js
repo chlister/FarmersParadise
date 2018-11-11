76ba3ef504a0ab6e93da508d22118883246b4b98
Marc Rohwedder Kær <zbcmakar16@zbc.dk>
2018-11-11 19:42:02 +0100

    JS can now get data and put it on the site

--- FarmersParadise/Scripts/App/app.js
+++ FarmersParadise/Scripts/App/app.js
@@ -1,23 +1,56 @@
 ﻿var currentFarm = {};
 var httpRequest = new XMLHttpRequest();
+var farmCreateBtn;
+var farmContainer;
+
+// this is needed because the .js file will load before the DOM
+window.onload = function () {
+    farmContainer = document.getElementById("js-farmContainer");
+    farmCreateBtn = document.getElementById("js-farmCreate");
+    farmCreateBtn.addEventListener("click", function () {
+        if ($("#farmName").val() === "") {
+            alert("Udfyld et navn til gården");
+        }
+        else {
+            createFarm();
+        }
+    });
+};
 
 function createFarm() {
     // TODO Webservice call
     currentFarm.FarmName = $("#farmName").val();
 
-    httpRequest.open('GET', 'http://localhost:53880/api/Farm');
+    // Subscribes to an event wich returns the JSON data
     httpRequest.onload = function () {
-        console.log(httpRequest.responseText);
+        var data = JSON.parse(httpRequest.responseText);
+        console.log(data[0].FarmName);
+        renderHTML(data);
     };
+    callWebservice('GET', 'Farm');
+    //httpRequest.send();
+
+}
+
+function renderHTML(data) {
+    var htmlString = "";
+
+    // Concatinates the information to a string
+    for (i = 0; i < data.length; i++) {
+        htmlString += "<p>" + data[i].FarmName + ".</p>";
+    }
+    // Adds the strings to the html page
+    farmContainer.insertAdjacentHTML("beforeend", htmlString);  
+}
+
+// Calls the specified APItarget with the method request.
+function callWebservice(method, APItarget) {
+    var url = 'http://localhost:53880/api/' + APItarget;
+    httpRequest.open(method, url);
     httpRequest.send();
-
 }
 
-function callWebservice() {
-    
-}
 
 $(document).ready(function () {
     console.info("Ready");
-    createFarm();
 });
