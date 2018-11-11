6e75e8f103fead9a55ceab70f7cc257e7254f769
Marc Rohwedder Kær <zbcmakar16@zbc.dk>
2018-11-11 11:30:47 +0100

    Added JS file for index

---
+++ FarmersParadise/Scripts/App/app.js
@@ -0,0 +1,23 @@
+﻿var currentFarm = {};
+var httpRequest = new XMLHttpRequest();
+
+function createFarm() {
+    // TODO Webservice call
+    currentFarm.FarmName = $("#farmName").val();
+
+    httpRequest.open('GET', 'http://localhost:53880/api/Farm');
+    httpRequest.onload = function () {
+        console.log(httpRequest.responseText);
+    };
+    httpRequest.send();
+
+}
+
+function callWebservice() {
+    
+}
+
+$(document).ready(function () {
+    console.info("Ready");
+    createFarm();
+});
